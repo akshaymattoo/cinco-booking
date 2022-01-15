61705b8bb75a1c6d0f9817c97f653dcfa65f58ac
akshay <amattoo@us.ibm.com>
2022-01-15 00:52:05 -0800

    updated closing of context and browser

--- src/weekend/weekend.js
+++ src/weekend/weekend.js
@@ -12,19 +12,15 @@ router.get('/', async (req, res, next) => {
 // This is api call for fetch all the experiments
 router.get('/ws1', async (req, res, next) => {
   console.log('starting the ws1 cron job');
-  let p,b,c;
   try {
     task1 = cron.schedule('0 7 * * 6', async () => {
     let obj = await common.open(process.env.SUNIL_USERNAME,process.env.SUNIL_PASSWORD);
-    p=obj.page;
-    b=obj.browser;
-    c=obj.context;
-    common.book(p,'tr:nth-child(4) td:nth-child(8)','7','17'); // ws1 8-9
-    common.book(p,'tr:nth-child(6) td:nth-child(8)','7','19'); // ws1 9-10
+    common.book(obj.page,'tr:nth-child(4) td:nth-child(8)','7','17'); // ws1 8-9
+    common.book(obj.page,'tr:nth-child(6) td:nth-child(8)','7','19'); // ws1 9-10
     await common.sleep(90000);
     console.log("ws1 done");
-    b.close();
-    c.close();
+    obj.browser.close();
+    obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
@@ -37,19 +33,15 @@ router.get('/ws1', async (req, res, next) => {
 
 router.get('/ws2', async (req, res, next) => {
   console.log('starting the ws2 cron job');
-  let p,b,c;
   try {
     task2 = cron.schedule('0 7 * * 6', async () => {
       let obj = await common.open(process.env.RAM_USERNAME,process.env.RAM_PASSWORD);
-      p = obj.page;
-      b=obj.browser;
-      c=obj.context;
-      common.book(p,'tr:nth-child(4) td:nth-child(9)','8','17'); // ws2 8-9
-      common.book(p,'tr:nth-child(6) td:nth-child(9)','8','19'); // ws2 9-10
+      common.book(obj.page,'tr:nth-child(4) td:nth-child(9)','8','17'); // ws2 8-9
+      common.book(obj.page,'tr:nth-child(6) td:nth-child(9)','8','19'); // ws2 9-10
       await common.sleep(90000);
       console.log("ws2 done");
-      b.close();
-      c.close();
+      obj.browser.close();
+      obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
@@ -62,19 +54,15 @@ router.get('/ws2', async (req, res, next) => {
 
 router.get('/ws3', async (req, res, next) => {
   console.log('starting the ws3 cron job');
-  let p,b,c;
   try {
     task3 = cron.schedule('0 7 * * 6', async () => {
       let obj = await common.open(process.env.ANUSHA_USERNAME,process.env.ANUSHA_PASSWORD);
-      p = obj.page;
-      b=obj.browser;
-      c=obj.context;
-      common.book(p,'tr:nth-child(4) td:nth-child(10)','9','17'); // ws3 8-9
-      common.book(p,'tr:nth-child(6) td:nth-child(10)','9','19'); // ws3 9-10
+      common.book(obj.page,'tr:nth-child(4) td:nth-child(10)','9','17'); // ws3 8-9
+      common.book(obj.page,'tr:nth-child(6) td:nth-child(10)','9','19'); // ws3 9-10
       await common.sleep(90000);
       console.log("ws3 done");
-      b.close();
-      c.close();
+      obj.browser.close();
+      obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
@@ -87,19 +75,15 @@ router.get('/ws3', async (req, res, next) => {
 
 router.get('/ws4', async (req, res, next) => {
   console.log('starting the ws4 cron job');
-  let p,b,c;
   try {
     task4 = cron.schedule('0 7 * * 6', async () => {
       let obj =  await common.open(process.env.RAVI_USERNAME,process.env.RAVI_PASSWORD);
-      p = obj.page;
-      b=obj.browser;
-      c=obj.context;
-      common.book(p,'tr:nth-child(4) td:nth-child(11)','10','17'); // ws4 8-9
-      common.book(p,'tr:nth-child(6) td:nth-child(11)','10','19'); // ws4 9-10
+      common.book(obj.page,'tr:nth-child(4) td:nth-child(11)','10','17'); // ws4 8-9
+      common.book(obj.page,'tr:nth-child(6) td:nth-child(11)','10','19'); // ws4 9-10
       await common.sleep(90000);
       console.log("ws4 done");
-      b.close();
-      c.close();
+      obj.browser.close();
+      obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
@@ -113,19 +97,15 @@ router.get('/ws4', async (req, res, next) => {
 
 router.get('/ws5', async (req, res, next) => {
   console.log('starting the ws1 10-11 ws2 10-11 cron job');
-  let p,b,c;
   try {
     task5 = cron.schedule('0 7 * * 6', async () => {
       let obj =  await common.open(process.env.ANAND_USERNAME,process.env.ANAND_PASSWORD);
-      p = obj.page;
-      b=obj.browser;
-      c=obj.context;
-      common.book(p,'tr:nth-child(8) td:nth-child(8)','7','21'); // ws1 10-11
-      common.book(p,'tr:nth-child(8) td:nth-child(9)','8','21'); // ws2 10-11
+      common.book(obj.page,'tr:nth-child(8) td:nth-child(8)','7','21'); // ws1 10-11
+      common.book(obj.page,'tr:nth-child(8) td:nth-child(9)','8','21'); // ws2 10-11
       await common.sleep(90000);
       console.log("ws5 done");
-      b.close();
-      c.close();
+      obj.browser.close();
+      obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
@@ -138,19 +118,15 @@ router.get('/ws5', async (req, res, next) => {
 
 router.get('/ws6', async (req, res, next) => {
   console.log('starting the ws3 10-11 ws4 10-11 cron job');
-  let p,b,c;
   try {
     task6 = cron.schedule('0 7 * * 6', async () => {
       let obj =  await common.open(process.env.UDAYA_USERNAME,process.env.UDAYA_PASSWORD);
-      p = obj.page;
-      b=obj.browser;
-      c=obj.context;
-      common.book(p,'tr:nth-child(8) td:nth-child(10)','9','21'); // ws3 10-11
-      common.book(p,'tr:nth-child(8) td:nth-child(11)','10','21'); // ws4 10-11
+      common.book(obj.page,'tr:nth-child(8) td:nth-child(10)','9','21'); // ws3 10-11
+      common.book(obj.page,'tr:nth-child(8) td:nth-child(11)','10','21'); // ws4 10-11
       await common.sleep(90000);
       console.log("ws6 done");
-      b.close();
-      c.close();
+      obj.browser.close();
+      obj.context.close();
     }, {
       scheduled: true,
       timezone: "America/Los_Angeles"
